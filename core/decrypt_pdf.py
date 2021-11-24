000c3e84fef7ba24dffd6dc6aa3e01b12827b02f
abdoalissa <35194512+abdoalissa@users.noreply.github.com>
2021-11-24 06:02:06 -0400

    Add files via upload

---
+++ core/decrypt_pdf.py
@@ -0,0 +1,16 @@
+from PyPDF2.pdf import PdfFileReader, PdfFileWriter
+from PyPDF2.utils import PdfReadError
+
+
+async def decrypt_pdf(path, output, password: str):
+    reader = PdfFileReader(path)
+    writer = PdfFileWriter()
+    reader.decrypt(password)
+    try:
+        for num in range(reader.numPages):
+            writer.addPage(reader.getPage(num))
+        with open(output, 'wb') as f:
+            writer.write(f)
+        return True
+    except PdfReadError:
+        return False
---
+++ core/merge_pdfs.py
@@ -0,0 +1,11 @@
+from PyPDF2.pdf import PdfFileReader, PdfFileWriter
+
+
+async def merge_pdfs(paths, output):
+    writer = PdfFileWriter()
+    for path in paths:
+        reader = PdfFileReader(path)
+        for num in range(reader.numPages):
+            writer.addPage(reader.getPage(num))
+    with open(output, 'wb') as f:
+        writer.write(f)
