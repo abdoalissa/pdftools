0e911156d1db8693d02808f612d85c09b3a0aae6
abdoalissa <35194512+abdoalissa@users.noreply.github.com>
2021-11-24 06:09:48 -0400

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,9 @@
 
+from .decrypt_pdf import decrypt_pdf
+from .encrypt_pdf import encrypt_pdf
+from .extract_info import extract_info
+from .image_to_pdf import images_to_pdf
+from .merge_pdfs import merge_pdfs
+from .rotate_pdf import rotate_pdf
+from .extract_text import extract_text
+from .split_pdf import split_pdf
